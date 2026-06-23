85a65f843a3a0db05ecae87d959170529402c5a2
dice2005x3005 <marcoencino1@gmail.com>
2026-06-22 18:56:58 -0700

    Se añadio un ciclo REPL basico

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		next := scanner.Scan()
+		if next != true {
+			fmt.Errorf("There is no token to scan")
+			continue
+		}
+		token := scanner.Text()
+		text := cleanInput(token)
+		fmt.Println("Your command was:", text[0])
+	}
 }
